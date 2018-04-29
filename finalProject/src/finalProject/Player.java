c3e5b66a3958a510e820c0c4eaea440bda6a499d
jiacheng xu <xujo@bc.edu>
2018-04-29 15:29:47 -0400

    add new playerinfo class

--- finalProject/src/finalProject/Player.java
+++ finalProject/src/finalProject/Player.java
@@ -1,12 +1,16 @@
 package finalProject;
 
+import java.net.*;
+import java.io.*;
+import java.util.*;
+
+
 public class Player {
 
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
 		
-		
-		
+			
 	}
 
 }
---
+++ finalProject/src/finalProject/playerInfo.java
@@ -0,0 +1,21 @@
+package finalProject;
+
+public class playerInfo {
+
+	int identifier = -1;
+	int port_num;
+	String IP;
+	
+	
+	public void setIdentifier(int i ) {
+		identifier = i;
+	}
+	public int getIdentifier() { 
+		return identifier;
+	}
+	
+	
+	
+	public void setPortNum() {}
+	
+}
